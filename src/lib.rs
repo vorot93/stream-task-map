4d0b5c30358388e8ffc67c4bf6e75c6f75b75518
Artem Vorotnikov <artem@vorotnikov.me>
2020-03-29 19:23:27 +0300

    Return finished/terminated streams via StreamTaskMap's Stream

--- src/lib.rs
+++ src/lib.rs
@@ -17,13 +17,24 @@ struct StreamTaskEntry {
     dropper: tokio::sync::oneshot::Sender<()>,
 }
 
-pub struct StreamTaskMap<K, S, V> {
+pub enum StreamYield<S>
+where
+    S: Stream,
+{
+    Item(S::Item),
+    Finished(S),
+}
+
+pub struct StreamTaskMap<K, S, V>
+where
+    S: Stream,
+{
     tasks: HashMap<K, StreamTaskEntry>,
 
-    sender: tokio::sync::mpsc::Sender<(K, Option<V>)>,
-    general_output: tokio::sync::mpsc::Receiver<(K, Option<V>)>,
+    sender: tokio::sync::mpsc::Sender<(K, StreamYield<S>)>,
+    general_output: tokio::sync::mpsc::Receiver<(K, StreamYield<S>)>,
 
-    _phantom: PhantomData<S>,
+    _marker: PhantomData<V>,
 }
 
 impl<K, S, V> Default for StreamTaskMap<K, S, V>
@@ -43,6 +54,7 @@ where
     S: Stream<Item = V> + Send + Unpin + 'static,
     V: Send + 'static,
 {
+    /// Create empty `StreamTaskMap`.
     #[must_use]
     pub fn new() -> Self {
         let (sender, general_output) = tokio::sync::mpsc::channel(0);
@@ -51,10 +63,11 @@ where
             tasks: Default::default(),
             sender,
             general_output,
-            _phantom: Default::default(),
+            _marker: Default::default(),
         }
     }
 
+    /// Spawn stream in a separate task and add it to this `StreamTaskMap`.
     pub fn insert(&mut self, key: K, mut stream: S) {
         let Self { sender, tasks, .. } = self;
         tasks.entry(key.clone()).or_insert_with(|| {
@@ -69,10 +82,18 @@ where
                         let fut = async {
                             let v = stream.next().await;
                             let is_none = v.is_none();
-                            let general_dropped = sender.send((key.clone(), v)).await.is_err();
+                            let mut general_dropped = false;
+                            if let Some(v) = v {
+                                general_dropped = sender
+                                    .send((key.clone(), StreamYield::Item(v)))
+                                    .await
+                                    .is_err();
+                            }
                             is_none || general_dropped
                         };
 
+                        // NOTE: if we await for general output while drop_handle is activated
+                        // then the in-flight item may be lost.
                         let finished = tokio::select! {
                             _ = &mut drop_handle => {
                                 true
@@ -83,6 +104,7 @@ where
                         };
 
                         if finished {
+                            let _ = sender.send((key.clone(), StreamYield::Finished(stream)));
                             return;
                         }
                     }
@@ -93,7 +115,8 @@ where
         });
     }
 
-    pub fn remove(&mut self, key: &K) {
+    /// Request stream termination. It will be returned as `StreamYield::Done`.
+    pub fn terminate(&mut self, key: &K) {
         self.tasks.remove(key);
     }
 }
@@ -102,16 +125,16 @@ impl<K, S, V> Stream for StreamTaskMap<K, S, V>
 where
     K: Clone + Eq + Hash + Send + Sync + Unpin + 'static,
     S: Stream<Item = V> + Send + Unpin + 'static,
-    V: Send + 'static,
+    V: Send + Unpin + 'static,
 {
-    type Item = (K, Option<V>);
+    type Item = (K, StreamYield<S>);
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let this = self.get_mut();
         Pin::new(&mut this.general_output).poll_next(cx).map(|res| {
             if let Some((k, v)) = &res {
-                if v.is_none() {
-                    this.remove(k);
+                if let StreamYield::Finished(_) = &v {
+                    this.terminate(k);
                 }
             }
 
